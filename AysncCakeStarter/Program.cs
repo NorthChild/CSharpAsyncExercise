d5ff4561b65e0be276c1944da4ada2bd116fac4e
michael carini <snownkc@icloud.com>
2021-09-08 16:39:32 +0100

    added async exercise code

---
+++ AysncCakeStarter/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AysncCake
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my birthday party");
+            HaveAPartyAsync();
+            Console.WriteLine("Thanks for a lovely party");
+            Console.ReadLine();
+        }
+
+        private async static void HaveAPartyAsync()
+        {
+            var name = "Cathy";
+            var cakeTask = BakeCakeAsync();
+            PlayPartyGames();
+            OpenPresents();
+            var cake = await cakeTask;
+            Console.WriteLine($"Happy birthday, {name}, {cake}!!");
+        }
+
+        private async static Task<Cake> BakeCakeAsync()
+        {
+            Console.WriteLine("Cake is in the oven");
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            //Thread.Sleep(TimeSpan.FromSeconds(5));
+            Console.WriteLine("Cake is done");
+            return new Cake();
+        }
+
+        private static void PlayPartyGames()
+        {
+            Console.WriteLine("Starting games");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            Console.WriteLine("Finishing Games");
+        }
+
+        private static void OpenPresents()
+        {
+            Console.WriteLine("Opening Presents");
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            Console.WriteLine("Finished Opening Presents");
+        }
+    }
+
+    public class Cake
+    {
+        public override string ToString()
+        {
+            return "Here's a cake";
+        }
+    }
+}
